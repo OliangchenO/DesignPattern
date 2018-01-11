e7127a930f3c2f2b6683a62dc368702884c3b164
OliangchenO <liang_chen@aliyun.com>
2018-01-11 15:31:36 +0800

    适配器模式

---
+++ src/com/adapterPattern/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.adapterPattern;
+
+public interface MediaPlayer {
+	public void play(String audioType, String fileName);
+}
---
+++ src/com/adapterPattern/VlcPlayer.java
@@ -0,0 +1,17 @@
+package com.adapterPattern;
+
+public class VlcPlayer implements AdvancedMediaPlayer {
+
+	@Override
+	public void playVlc(String fileName) {
+		// TODO Auto-generated method stub
+		System.out.println("Playing vlc file. Name: "+ fileName);
+	}
+
+	@Override
+	public void playMp4(String fileName) {
+		// TODO Auto-generated method stub
+		//什么也不做
+	}
+
+}
